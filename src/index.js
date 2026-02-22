10330537429e2fdd8ec449ebaecbb9c4e76fdece
mansi-okay <mansiagrawat2@gmail.com>
2026-02-23 00:57:30 +0530

    utilities added

--- src/index.js
+++ src/index.js
@@ -5,16 +5,13 @@ import dotenv from "dotenv"
 dotenv.config()
 import {app} from "./app.js"
 import {connectDB} from "./config/db.js"
-import { response } from "express";
-import { error } from "node:console";
 
 connectDB()
 .then((response) => {
     app.listen(process.env.PORT, () => {
         console.log(`Express server started... Running on port: ${process.env.PORT}`);
-        
     })
 })
 .catch((error) => {
-    console.log("DB connection failed");
+    console.log(`DB connection failed: ${error}`);
 })
